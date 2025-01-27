af85caa355f186b2486c92fbcc0fc81cb7ff245f
manikanta17815 <manimruhper@gmail.com>
2025-01-27 11:20:19 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+integer_var = 10
+float_var = 3.14
+string_var = "Hello, World!"
+boolean_var = True
+
+print("Integer Variable: ", integer_var, ", Type: ", type(integer_var))
+print("Float Variable: ", float_var, ", Type: ", type(float_var))
+print("String Variable: ", string_var, ", Type: ", type(string_var))
+print("Boolean Variable: ", boolean_var, ", Type: ", type(boolean_var))
+
+
+# In[ ]:
+
+
+
+
